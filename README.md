9309571aaaf7e48799d33eebc5092cac9d1c7223
Nathan Beck <njbeck92@gmail.com>
2019-04-10 03:21:59 -0500

    added config and more motherboard selections

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # pcpartpicker-scraper
 
-Scrapes the motherboard section of pcpartpicker.com for z370 and z390 boards
-* Units that drop below a defined price (triggerPrice) will trigger an email alert
-* Add unique terms to excludedUnits list to exclude units you don't want alerts about
+Scrapes the motherboard section of pcpartpicker.com for selected boards
+* Customizable using a config
+* Select the types of boards you want (only works for first page of results for now)
+* Units that drop below a defined price will trigger an email alert
+* Add unique terms to exclude units you don't want alerts about
 * To be run periodically using a task scheduler
--- pcpp mobo scraper.py
+++ pcpp mobo scraper.py
@@ -6,47 +6,88 @@
 # check sites to make sure price has actually dropped
 # log data that is scraped to check for unusual deals
 
-import requests
 from bs4 import BeautifulSoup
 import re
 import smtplib
-from requests_html import HTMLSession
+import os
+import configparser
+from requests_html import HTMLSession, HTML
 # using requests_html because page uses javascript 
 # (first run will download/install chromium to render the javascript)
 
-pageURL = "https://pcpartpicker.com/products/motherboard/detailed-list/#c=128,135"
-# url for z370 and z390 motherboards
-triggerPrice = 100
-# price which triggers an email if a matching unit price falls below
-excludedUnits = []
-# some case insensitive unit-unique terms to exclude units I don't care about
-# example: 'pro4'
+pageURL = "https://pcpartpicker.com/products/motherboard/detailed-list/#c="
+unitSuffixes = {
+    'z370': '128', 'z390': '135', 'z270': '119', 'z170': '110', 'h370': '130',
+    'h310': '131', 'h270': '121', 'h170': '111', 'h110': '113', 'x299': '126',
+    'b360': '129', 'b250': '120', 'b350': '124', 'b450': '133', 'x370': '123',
+    'x399': '127', 'x470': '132'
+    }
 
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+triggerPrice = float(config['price and terms']['triggerPrice'])
+excludedUnits = config['price and terms']['excludedTerms'].split(', ')
+
+# next few if statements are grabbing email info from environment variables
+# if told to; otherwise, just take them straight from the config
 sendingEmail = ''
-sendingPassword = ''
-# email and password of the account you want to send from
-# I suggest using app unique passwords hidden in environment variables
+if config['email info'].getboolean('usingEnvSendEmail'):
+    sendingEmail = os.environ.get(config['email info']['sendingEmail'])
+else: 
+    sendingEmail = config['email info']['sendingEmail']
+
+    sendingPassword = ''
+if config['email info'].getboolean('usingEnvPassword'):
+    sendingPassword = os.environ.get(config['email info']['sendingPassword'])
+else: 
+    sendingPassword = config['email info']['sendingPassword']
 
 receivingEmail = ''
-# i use the same email to receive but it can of course be changed
+if config['email info'].getboolean('usingEnvReceiveEmail'):
+    receivingEmail = os.environ.get(config['email info']['receivingEmail'])
+else: 
+    receivingEmail = config['email info']['receivingEmail']
 
+sendingPort = config['email info'].getint('sendingEmailPort')
 
+chosenSuffixes = config['price and terms']['units'].split(', ')
 
+# we are appending the relevant values from the unitSuffixes dict to the URL
+urlSuffixes = []
+for suffix in chosenSuffixes:
+    urlSuffixes.append(unitSuffixes[suffix])
+pageURL = pageURL + ','.join(urlSuffixes) + '&sort=price'
 
+def email_alert(alertDict):
+    '''logs in and sends and email with the info for the found units'''
+	with smtplib.SMTP('smtp.gmail.com', sendingPort) as smtp:
+		smtp.ehlo()
+		smtp.starttls()
+		smtp.ehlo()
+		smtp.login(sendingEmail, sendingPassword)
+		
+        subject = 'motherboard price drop'
+        body = ''
+		for name in alertDict.keys():
+			body += name + ' is at $' + str(alertDict[name]) + '\n'
+        msg = f'Subject: {subject}\n\n{body}'
+        
+        smtp.sendmail(sendingEmail, receivingEmail, msg)
+
+# get our html after javascript has finished rendering
 session = HTMLSession()
 r = session.get(pageURL)
 r.html.render()
 source = r.html.html
-# returns our html after javascript has finished rendering
 
 soup = BeautifulSoup(source, 'lxml')
 
-# relevant info is tagged td and have classes tdname and tdprice respectively
+# names/prices tagged td and have classes tdname and tdprice respectively
 namematch = soup.find_all('td', class_='tdname')
 names = []
 for name in namematch:
 	names.append(name.a.text)
-
 
 pricematch = soup.find_all('td', class_='tdprice')
 prices = []
@@ -56,45 +97,23 @@ for price in pricematch:
 	else:
 		prices.append('N/A')
 
-# urls = []
-# for url in namematch:
-# 		urls.append(url.a.get('href'))
-# will probably use this eventually
-
-pattern = "(" + ")|(".join(excludedUnits).lower() + ")"
-# regex pattern for our exclusionary terms
-
-
+# generate dict of all name: price where price isnt N/A
 fullDict = {k: v for k, v in zip(names, prices)}
 cleanDict = {}
 for name in fullDict.keys():
 	if fullDict[name] != 'N/A':
 		cleanDict[name] = fullDict[name]
-# generate dict of all name: price where price isnt N/A
 
+# regex pattern for our exclusionary terms
+pattern = "(" + ")|(".join(excludedUnits).lower() + ")"
+
+# generate dictionary of name: price which meets our criteria
 alertDict = {}
 for name in cleanDict.keys():
 	if cleanDict[name] < triggerPrice:
 		if not re.search(pattern, name.lower()):
 			alertDict[name] = cleanDict[name]
-# generate dictionary of name: price which meets our criteria
-
-def emailAlert(alertDict):
-	with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
-		smtp.ehlo()
-		smtp.starttls()
-		smtp.ehlo()
-		smtp.login(sendingEmail, sendingPassword)
-		subject = 'motherboard price drop'
-
-		body = ''
-		for name in alertDict.keys():
-			body += name + ' is at $' + str(alertDict[name]) + '\n'
-
-		msg = f'Subject: {subject}\n\n{body}'
-
-		smtp.sendmail(sendingEmail, receivingEmail, msg)
 
 
 if alertDict:
-	emailAlert(alertDict)
+	print(alertDict)
