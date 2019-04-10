93b3ea99dec05e695bdcb6f8b600f02facfb74ee
Nathan Beck <njbeck92@gmail.com>
2019-04-10 04:58:05 -0500

    added cpu and gpu scraping

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # pcpartpicker-scraper
 
-Scrapes the motherboard section of pcpartpicker.com for selected boards
+Scrapes the motherboard, cpu, or gpu sections of pcpartpicker.com for selected units
 * Customizable using a config
-* Select the types of boards you want (only works for first page of results for now)
+* Select the types of units you want (only works for first page of results for now)
 * Units that drop below a defined price will trigger an email alert
 * Add unique terms to exclude units you don't want alerts about
 * To be run periodically using a task scheduler
--- pcpp mobo scraper.py
+++ pcpp mobo scraper.py
@@ -1,7 +1,7 @@
 
 # todo: 
-# add error handling on the rendering
-# generalize to other product sections/motherboards types
+# add error handling on the requests and rendering
+# add more specific search terms particularly for cpu/gpu
 # exclude other properties such as vendors
 # check sites to make sure price has actually dropped
 # log data that is scraped to check for unusual deals
@@ -15,49 +15,57 @@ from requests_html import HTMLSession, HTML
 # using requests_html because page uses javascript 
 # (first run will download/install chromium to render the javascript)
 
-pageURL = "https://pcpartpicker.com/products/motherboard/detailed-list/#c="
-unitSuffixes = {
+pageURL = 'https://pcpartpicker.com/products/'
+
+moboSuffixes = {
     'z370': '128', 'z390': '135', 'z270': '119', 'z170': '110', 'h370': '130',
     'h310': '131', 'h270': '121', 'h170': '111', 'h110': '113', 'x299': '126',
     'b360': '129', 'b250': '120', 'b350': '124', 'b450': '133', 'x370': '123',
     'x399': '127', 'x470': '132'
-    }
+}
+
+cpuSuffixes = {
+    'i5': '12', 'i7': '13', 'i3': '11', 'i9': '61', 'ryzen3': '62',
+    'ryzen5': '60', 'ryzen7': '59'
+}
+
+gpuSuffixes = {
+    '1050': '379', '1050 ti': '380', '1060 3gb': '378', '1060 6gb': '373',
+    '1070': '369', '1070 ti': '415', '1080': '367', '1080 ti': '390',
+    '1660': '439', '1660 ti': '438', '2060': '436', '2070': '425',
+    '2080': '427', '2080 ti': '424', 'titan rtx': '435',
+    'rx 560 - 1024': '395', '570': '392', '580': '391', '590': '431',
+    'vega 56': '404', 'vega 64': '405', 'radeon vii': '437',
+    'rx 560 - 896': '416',
+}
+
+unitSuffixes = {}
 
 config = configparser.ConfigParser()
 config.read('config.ini')
 
-triggerPrice = float(config['price and terms']['triggerPrice'])
-excludedUnits = config['price and terms']['excludedTerms'].split(', ')
-
-# next few if statements are grabbing email info from environment variables
-# if told to; otherwise, just take them straight from the config
-sendingEmail = ''
-if config['email info'].getboolean('usingEnvSendEmail'):
-    sendingEmail = os.environ.get(config['email info']['sendingEmail'])
-else: 
-    sendingEmail = config['email info']['sendingEmail']
-
-    sendingPassword = ''
-if config['email info'].getboolean('usingEnvPassword'):
-    sendingPassword = os.environ.get(config['email info']['sendingPassword'])
-else: 
-    sendingPassword = config['email info']['sendingPassword']
-
-receivingEmail = ''
-if config['email info'].getboolean('usingEnvReceiveEmail'):
-    receivingEmail = os.environ.get(config['email info']['receivingEmail'])
-else: 
-    receivingEmail = config['email info']['receivingEmail']
-
-sendingPort = config['email info'].getint('sendingEmailPort')
+# form the URL from the lookingFor config and the dictionaries
+lookingFor = config['price and terms']['lookingFor'].lower()
+if lookingFor == 'processor' or 'cpu':
+    unitSuffixes = cpuSuffixes
+    pageURL += 'cpu' + '/#s='
+elif lookingFor == 'gpu':
+    unitSuffixes = gpuSuffixes
+    pageURL += 'video-card' + '/#c='
+else:
+    unitSuffixes = moboSuffixes
+    pageURL += 'motherboard' + '/#c='
 
 chosenSuffixes = config['price and terms']['units'].split(', ')
-
-# we are appending the relevant values from the unitSuffixes dict to the URL
 urlSuffixes = []
 for suffix in chosenSuffixes:
     urlSuffixes.append(unitSuffixes[suffix])
+
 pageURL = pageURL + ','.join(urlSuffixes) + '&sort=price'
+
+triggerPrice = float(config['price and terms']['triggerPrice'])
+
+excludedUnits = config['price and terms']['excludedTerms'].split(', ')
 
 def email_alert(alertDict):
     '''logs in and sends and email with the info for the found units'''
